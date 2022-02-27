d54268e403aae27807a57c31c487d3fbc32070e9
John T. Wodder II <git@varonathe.org>
2022-02-27 16:07:09 -0500

    Fix my mistakes

---
+++ src/foobar/widgets/__init__.py
@@ -0,0 +1,5 @@
+from .base import Widget, WidgetSpec
+from .blue import BlueWidget
+from .red import RedWidget
+
+__all__ = ["BlueWidget", "RedWidget", "Widget", "WidgetSpec"]
--- src/foobar/widgets/blue.py
+++ src/foobar/widgets/blue.py
@@ -7,6 +7,6 @@ class BlueWidget(Widget):
     nessness: int = attr.field(init=False)
 
     def __attrs_post_init__(self) -> None:
-        if self.app.nessess is None:
+        if self.spec.nessness is None:
             raise ValueError("Blue widgets must be nessy")
-        self.nessness = self.app.nessness
+        self.nessness = self.spec.nessness
